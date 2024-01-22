3f74d5bceac849e1c50906482ce2ca16389613f0
Victor Tancrez <62173957+vtancrez@users.noreply.github.com>
2024-01-22 19:12:31 +0100

    late fusion fonctionnel

--- src/late/combine_model.py
+++ src/late/combine_model.py
@@ -6,11 +6,10 @@ class Combine_prediction(nn.Module):
     def __init__(self):
         super(Combine_prediction, self).__init__()
         # Définir les couches ici
-        self.fc1 = nn.Linear(4, 10)  # 4 caractéristiques en entrée, 10 en sortie
-        self.fc2 = nn.Linear(10, 2)  # 10 caractéristiques en entrée, 2 en sortie
+        self.fc1 = nn.Linear(4, 2)  # 4 caractéristiques en entrée, 2 en sortie
+        
 
     def forward(self,x):
-        # Concaténer les vecteurs d'entrée
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)
+        x = self.fc1(x)
+ 
         return x
--- src/pipeline_late_fusion.py
+++ src/pipeline_late_fusion.py
@@ -18,6 +18,14 @@ def training_audio_text_late_model(num_epochs,seed,model_name,train_loader_audio
         model_text=training_model_text(num_epochs,seed,model_name,train_loader_text,val_loader_text,patience,class_weight=class_weight,task=task,save=save)
         model_audio=training_model_audio(num_epochs,seed,model_name,train_loader_audio,val_loader_audio,patience=patience,class_weight=class_weight,task=task,save=save)
     else:
+        
+        if os.path.isfile(f'../modele/text_model/{task}/{model_name}')==False:
+            print("Text model not found , training it")
+            model_text=training_model_text(num_epochs,seed,model_name,train_loader_text,val_loader_text,patience,class_weight=class_weight,task=task,save=save)
+        if os.path.isfile(f'../modele/audio_model/{task}/{model_name}')==False:
+            print("Audio model not found , training it")
+            model_audio=training_model_audio(num_epochs,seed,model_name,train_loader_audio,val_loader_audio,patience=patience,class_weight=class_weight,task=task,save=save)
+            
         model_text=torch.load(f'../modele/text_model/{task}/{model_name}')
         model_audio=torch.load(f'../modele/audio_model/{task}/{model_name}')   
         
@@ -30,10 +38,27 @@ def training_audio_text_late_model(num_epochs,seed,model_name,train_loader_audio
     combine_model.to(device)
     loss_fn = nn.CrossEntropyLoss(weight=class_weights)
     combine_model=training_late_combinaison(num_epochs, optimizer, model_audio, model_text, combine_model, loss_fn, train_loader_audio, train_loader_text, val_loader_audio, val_loader_text, device, model_name, task, patience, save=True)
+    return combine_model,model_audio,model_text
         
         
+def evaluate_model_audio_text_late(combine_model,model_audio,model_text,test_loader_audio,test_loader_text):
+    #evaluation
+    f1,conf_matrix=prediction_late_combinaison(combine_model,model_audio,model_text,test_loader_audio,test_loader_text)
+    print(f'Test F1 Score: {f1}')
+    print(f'Confusion Matrix:\n{conf_matrix}')
+
+    total_class_0 = np.sum(conf_matrix[0])
+    total_class_1 = np.sum(conf_matrix[1])
+    detected_class_0 = conf_matrix[0, 0]  # Vrais positifs pour la classe 0
+    detected_class_1 = conf_matrix[1, 1]  # Vrais positifs pour la classe 1
+
+    print(f'Nombre d\'éléments de classe 0 détectés : {detected_class_0} sur {total_class_0}')
+    print(f'Nombre d\'éléments de classe 1 détectés : {detected_class_1} sur {total_class_1}')
+
 
 
 def training_evaluate_model_audio_text_late(num_epochs,seed,model_name,patience,class_weight=[1.0,5.0],task="yield",save=True,all_training=False):
     train_loader_audio,val_loader_audio,test_loader_audio,train_loader_text,val_loader_text,test_loader_text=load_data_audio_text_late(seed,task)
-    training_audio_text_late_model(num_epochs,seed,model_name,train_loader_audio,val_loader_audio,test_loader_audio,train_loader_text,val_loader_text,test_loader_text,patience,class_weight=class_weight,task=task,save=save,all_training=all_training)
+    combine_model,model_audio,model_text=training_audio_text_late_model(num_epochs,seed,model_name,train_loader_audio,val_loader_audio,test_loader_audio,train_loader_text,val_loader_text,test_loader_text,patience,class_weight=class_weight,task=task,save=save,all_training=all_training)
+    evaluate_model_audio_text_late(combine_model,model_audio,model_text,test_loader_audio,test_loader_text)
+    
